63d1670c8a39bd9a5fb0ce14c64b7ce6ea3e1159
Gustavo <kkowakskigustavo@gmail.com>
2025-06-27 23:10:15 -0300

    GrantedAuthority e UserDetails implementados e Seed do Database

--- src/main/java/com/devsuperior/bds04/entities/User.java
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -1,13 +1,17 @@
 package com.devsuperior.bds04.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -41,8 +45,6 @@ public class User {
         this.id = id;
     }
 
-  
-
     public String getEmail() {
         return email;
     }
@@ -51,8 +53,38 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
